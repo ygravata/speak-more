26fd6751d315749eb0c28bf4031c5f5756411be6
Yuri Gravata <ygravata@gmail.com>
2021-03-23 19:51:07 -0300

    adding new api route

--- app/controllers/api/v1/speak_localities_controller.rb
+++ app/controllers/api/v1/speak_localities_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::SpeakLocalitiesController < Api::V1::BaseController
+  before_action :set_speak_locality, only: [ :show ]
   def index
     @speak_localities = SpeakLocality.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_speak_locality
+    @speak_locality = SpeakLocality.find(params[:id])
   end
 end
