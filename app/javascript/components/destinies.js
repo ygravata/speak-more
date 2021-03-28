3d2f828dd6023810ecf88300f2219ed79c09cfa2
Yuri Gravata <ygravata@gmail.com>
2021-03-28 19:32:37 -0300

    cleaning code

--- app/javascript/components/destinies.js
+++ app/javascript/components/destinies.js
@@ -2,14 +2,11 @@ import Rails from '@rails/ujs';
 
 const generateDestiniesDown = () => {
   const originDropDown = document.getElementById("origin_dropdown")
-  console.log(originDropDown);
   if (originDropDown) {
     const wrapperHtml = document.getElementById("destiny_dropdown");
-
     originDropDown.addEventListener("change", (event) => {
       wrapperHtml.innerHTML = "";
       const originValue = originDropDown.options[originDropDown.options.selectedIndex].getAttribute('dataset');
-      console.log(originDropDown.options[originDropDown.options.selectedIndex].getAttribute('dataset'));
 
       Rails.ajax({
         url: ` /api/v1/speak_localities/${originValue}`,
@@ -18,16 +15,24 @@ const generateDestiniesDown = () => {
           console.log(data.speak_destinies);
           data.speak_destinies.forEach((destiny) => {
             console.log(destiny.code);
-              const element = `<option dataset="${destiny.id}">${destiny.code}</option>`;
-              wrapperHtml.insertAdjacentHTML("beforeend", element);
+            const element = `<option dataset="${destiny.id}">${destiny.code}</option>`;
+            wrapperHtml.insertAdjacentHTML("beforeend", element);
 
           })
-        const destinyLabel = document.querySelector("#destiny_label");
-        destinyLabel.classList.remove("invisible");
-        wrapperHtml.classList.remove("invisible");
         }
-
       })
+
+      wrapperHtml.classList.remove("invisible");
+
+      const destinyLabel = document.querySelector("#destiny_label");
+      const destinyDropdown = document.querySelector("#destiny_dropdown");
+      const planLabel = document.querySelector("#plan_label");
+      const planDropdown = document.querySelector("#plan_dropdown");
+
+      destinyLabel.classList.remove("invisible");
+      destinyDropdown.classList.remove("invisible");
+      planLabel.classList.remove("invisible");
+      planDropdown.classList.remove("invisible");
     })
   }
 }
